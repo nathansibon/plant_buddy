58a99273bf05cae79ecb9005f752a399f9cd4df5
nathansibon <65937157+nathansibon@users.noreply.github.com>
2020-06-28 17:36:39 -0700

    More oops

--- webpage_daily_data.py
+++
@@ -1,4 +0,0 @@
-yesterday_drybulb_max = '25'
-yesterday_drybulb_min = '19'
-yesterday_rh_max = '56'
-yesterday_rh_min = '42'
--- webpage_sensor_data.py
+++
@@ -1,6 +0,0 @@
-read_time = '17:09:04'
-indoor_drybulb = '24'
-indoor_rh = '46'
-
-outdoor_drybulb = '15'
-outdoor_rh = '42'
