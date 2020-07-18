4839359364f9aaf844f219ec6b2938afabce556f
krueger-matt <krueger.matt@gmail.com>
2020-07-18 16:25:15 -0700

    Added config variable to switch between Celsius and Fahrenheit

--- config.py
+++ config.py
@@ -2,6 +2,7 @@
 name = 'pi_1'   # This should correspond to the name of your pi and will define the name of the database
 pi_serial = 'XXXXXXXXXXXXXXXX'
 timezone = 'US/Pacific'     # see PYTZ docs for a full list of available timezones
+temp_scale = 'C' # Set temp scale to Celsius or Fahrenheit by using 'C' or 'F'. This will only change webserver outputs
 
 # Exterior info
 weather_api_key = 'XXXXXXXXXXXXXXXXXXXXXXX'
